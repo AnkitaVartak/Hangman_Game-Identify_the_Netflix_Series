c4c07b7b419f70620e778608c2dcc018aba16c7b
RohanRV <rv22894@gmail.com>
2020-05-02 16:21:03 +0530

    Update WORD_GAME_Siddhart.py

--- WORD_GAME_Siddhart.py
+++ WORD_GAME_Siddhart.py
@@ -16,7 +16,7 @@ class Application(Frame):
         """Initialize Frame."""
         Frame.__init__(self, master)
 
-        master.minsize(width=500, height=200)
+        master.minsize(width=200, height=100)
 
         self.list_done = []
 
@@ -32,58 +32,109 @@ class Application(Frame):
         self.list_done.append(qno)
 
         if self.number == 1:
-            self.question = "The word 'cruyrecn is jumbled. Guess the right word."
-            self.ans = "currency"
+            self.question = "A terminally ill Chemistry teacher teams with a former student to manufacture crystal meth to secure his family's future."
+            self.ans = "Breaking Bad"
 
         elif self.number == 2:
-            self.question = "Forwards I am heavy. Backwards I am not. What am I?"
-            self.ans = "ton"
+            self.question = "A privileged New Yorker ends up in a women's prison when a past crime catches up with her in this Emmy-winning series."
+            self.ans = "Orange is the New Black"
 
         elif self.number == 3:
-            self.question = "The more of me there is, the less you see. What am I?"
-            self.ans = "darkness"
+            self.question = "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl."
+            self.ans = "Stranger Things"
 
         elif self.number == 4:
-            self.question = "What holds water yet is full of holes?"
-            self.ans = "sponge"
+            self.question = "Set in 1960s New York City, this award-winning series takes a peek inside an ad agency during an era when the cutthroat business had a glamorous lure."
+            self.ans = "Mad Men"
 
         elif self.number == 5:
-            self.question = "I can be cracked, I can be made. I can be told and I can be played"
-            self.ans ="joke"
+            self.question = "In this updated take on Sir Arthur Conan Doyle's beloved mystery tales, the eccentric sleuth prowls the streets of modern London in search of clues."
+            self.ans ="Sherlock"
 
         elif self.number == 6:
-            self.question = "What loses its head every morning and gets it back every night?"
-            self.ans ="pillow"
+            self.question = "Rich, unreasonably attractive private school students do horrible, scandalous things to each other. Repeatedly."
+            self.ans ="Gossip Girl"
 
 
         elif self.number == 7:
-            self.question = "When you know me, I am nothing. What you don't know me, I am something. What am I?"
-            self.ans ="riddle"
+            self.question = "After a teenage girl's perplexing suicide, a classmate receives a series of tapes that unravel the mystery of her tragic choice."
+            self.ans ="13 Reasons Why"
 
 
         elif self.number == 8:
-            self.question = "What is it that you ought to keep after you have given it to someone else?"
-            self.ans ="promise"
+            self.question = "Love, laughter and the best friends you could ask for. Just like real life. But with really nice apartments."
+            self.ans ="F.R.I.E.N.D.S"
 
 
         elif self.number == 9:
-            self.question = "What kind of coat can be put on only when wet?"
-            self.ans ="paint"
+            self.question = "Mild-mannered man works for the police department as a blood-splatter analyst but moonlights as a murderer who only targets other killers."
+            self.ans ="Dexter"
 
+
+        elif self.number == 10:
+            self.question = "The true story of Columbia's infamously violent and powerful drug cartels fuels this gritty gangster drama series."
+            self.ans ="Narcos"
+
+
+        elif self.number == 11:
+            self.question = "The boss is an idiot, the workers are clueless, elaborate pranks and feuds abound. Just another day at the office."
+            self.ans ="The Office"
+
+
+        elif self.number == 12:
+            self.question = "A ruthless politician will stop at nothing to conquer Washington, D. C., in this Emmy and Golden Globe-winning political drama."
+            self.ans ="House of Cards"
+
+
+        elif self.number == 13:
+            self.question = "While navigating the troubled waters of sex, romance, school, and family, a teen and his gang become entangled in a dark mystery."
+            self.ans ="Riverdale"
+
+
+        elif self.number == 14:
+            self.question = "A notorious gang in 1919 Birmingham, England, is led by the fierce Tommy Shelby, a crime boss set on moving up in the world no matter the cost."
+            self.ans ="Peaky Blinders"
+
+            
+        elif self.number == 15:
+            self.question = "A super-powered woman who had a brief superhero career until an incident where the villainous Kilgrave caused her to kill someone. After that incident, she became a private investigator."
+            self.ans ="Jessica Jones"
+
+            
+        elif self.number == 16:
+            self.question = "The sleek world of tomorrow offers opportunities beyond our wildest dreams. At a price of our worse nightmares."
+            self.ans ="Black Mirror"
+
+            
+        elif self.number == 17:
+            self.question = "The story focuses on a seasoned and cynical Bombay police officer, summoned by an anonymous tip one morning, a voice which promises him an opportunity to capture a powerful criminal overlord of the G-Company. As the stakes mount, it becomes clear that the game the two players thought they were engaged in is in fact part of a much larger scenario, one that expands beyond their city."
+            self.ans ="Sacred Games"
+
+            
+        elif self.number == 18:
+            self.question = "One of Manhattan's top corporate lawyers sets out to recruit a new hotshot associate and hires the only guy that impresses him, a brilliant but unmotivated college dropout. Though he isn't actually a lawyer, this legal prodigy has the book smarts of a Harvard law grad and the street smarts of a hustler. However, in order to serve justice and save their jobs, both these unconventional thinkers must continue the charade."
+            self.ans ="Suits"
+
+            
+        elif self.number == 19:
+            self.question = "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan."
+            self.ans ="Money Heist"
+            
         else:
-            self.question = "He is known to commit a friendly home invasion one night a year, never taking but always leaving stuff behind."
-            self.ans = "santa claus"
+            self.question = "Family. Power. A young woman rises to rule an empire amid the challenges of the modern age."
+            self.ans ="The Crown"
+
 
     def create_widgets(self):
         """Program all the widgets to be used."""
         if self.qlabel is not None:
             self.qlabel.grid_forget()
 
-        self.qlabel = Label(self, text = self.question)
+        #self.qlabel = Label(self, text = self.question)
 
-        self.qlabel.grid(row=0, column=0, columnspan=2, sticky=W)
+        #self.qlabel.grid(row=0, column=0, columnspan=2, sticky=W)
 
-        #Label(self, text = self.question).grid(row=0, column=0, columnspan=2, sticky=W)
+        Label(self, text = self.question).grid(row=0, column=0, columnspan=2, sticky=W)
 
         Label(self,
               text="Try to guess the answer"
